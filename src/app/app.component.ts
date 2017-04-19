a52b4acf12a560db2549f94ea61a14a87fdf6044
lohmuss <caudery@gmail.com>
2017-04-19 09:50:11 +0300

    added material sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent  { 
   constructor ( ) {
+  }
+  
+  isScreenWide() {
+    let width: number = this.getScreenWidth();
+    if (width > 720) {
+      return true;
+    }
+    return false;
+  }
 
+  getScreenWidth(): number {
+    let width: number = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    return width;
   }
 }
